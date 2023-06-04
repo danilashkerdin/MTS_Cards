c661b0ee91205b2e2fa747f6427902236950b039
dashkerdin <dashkerdin@mts.ru>
2023-06-04 19:05:41 +0300

    initial commit Cards classes created

---
+++ Card.py
@@ -0,0 +1,49 @@
+import enum
+
+
+class Color(enum.StrEnum):
+    red = "red"
+    yellow = "yellow"
+    green = "green"
+    blue = "blue"
+
+
+class Action(enum.StrEnum):
+    reverse = "reverse"
+    get_2_cards = "get_2_cards"
+    skip = "skip"
+
+
+class SpecialAction(enum.StrEnum):
+    get_4_cards = "get_4_cards"
+    # change_color = "change_color"
+
+
+class Digit(enum.IntEnum):
+    zero = 0  # карт с нулями в колоде уно - 4шт, а всех остальных цифр - по 8
+    one = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+
+
+class Card:
+
+    def render(self):
+        pass
+
+
+class ColoredCard(Card):
+
+    color: Color
+    value: Action | Digit | None
+
+
+class SpecialCard(Card):
+    # всегда меняет цвет
+    actions: SpecialAction | None
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Simple Uno Game
+
+Rules got from <a href=https://ru.wikipedia.org/wiki/%D0%A3%D0%BD%D0%BE>here</a>
